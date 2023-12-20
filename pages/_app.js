f46940f659bf49796a378bbbb722a62ae48387c6
Franklin Yin <yin4thewin@gmail.com>
2023-12-19 19:08:29 -0500

    add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import '../styles/global.css';
+import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function App({ Component, pageProps }) {
     return <>
     <Component {...pageProps} />
     <SpeedInsights/>
+    <Analytics/>
     </>;
 }
